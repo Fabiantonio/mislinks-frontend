970dbdf7eb0d41da20bd62c8f4e6513451235430
Fabian Casas <fa.casas@duocuc.cl>
2026-02-05 23:45:18 -0300

    deploy commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     const location = useLocation()
 
   return (
-          <header className="sticky top-0 z-50 bg-white border-b border-slate-200 py-4 shadow-sm shadow-slate-100/50">
+          <header className="sticky top-0 z-50 bg-transparent border-b backdrop-blur-xl border-slate-200 py-4 shadow-sm shadow-slate-100/50">
             <div className="mx-auto max-w-5xl flex flex-col md:flex-row items-center md:justify-between px-5 lg:px-0">
               <div className="w-full md:w-1/3 flex justify-center md:justify-start">
                 <Logo />
